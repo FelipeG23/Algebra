2c9897965b38587b7ac14ab17e7d007393e68935
Felipe <gonzalezfelipe784@gmail.com>
2018-05-03 09:16:54 -0500

    03/05/2018 001
    Cambios para el nivel dos

--- src/nivelDos/NivelDosController.java
+++ src/nivelDos/NivelDosController.java
@@ -5,9 +5,12 @@
  */
 package nivelDos;
 
+import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
 /**
  * FXML Controller class
@@ -16,12 +19,65 @@ import javafx.fxml.Initializable;
  */
 public class NivelDosController implements Initializable {
 
+    @FXML
+    private Label numeroUno;
+    @FXML
+    private Label numeroDos;
+    @FXML
+    private Label numeroTres;
+    @FXML
+    private Label numeroCuatro;
+    @FXML
+    private Label numeroCinco;
+    @FXML
+    private Label numeroSeis;
+    @FXML
+    private Label numeroSiete;
+    @FXML
+    private Label numeroOcho;
+    @FXML
+    private Label numeroNueve;
+    @FXML
+    private Label numeroDiez;
+    @FXML
+    private Label numeroOnce;
+    @FXML
+    private Label numeroDoce;
+    @FXML
+    private Label numeroTrece;
+    @FXML
+    private Label numeroCatorce;
+    @FXML
+    private Label numeroQuince;
+    @FXML
+    private Label numeroDieceseis;
+    @FXML
+    private Label numeroDiecesiete;
+    @FXML
+    private Label numeroDieceocho;
+    @FXML
+    private JFXTextField valorUno;
+    @FXML
+    private JFXTextField valorDos;
+    @FXML
+    private JFXTextField valorTres;
+    @FXML
+    private JFXTextField valorCuatro;
+    @FXML
+    private JFXTextField valorCinco;
+    @FXML
+    private JFXTextField valorSeis;
+    @FXML
+    private JFXTextField valorSiete;
+    @FXML
+    private JFXTextField valorOcho;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
